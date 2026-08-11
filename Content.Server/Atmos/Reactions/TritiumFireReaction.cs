7d56cc7ba5385ab39bee33bdb10859bfed00a038
Chlorinight <ppoppo26511@gmail.com>
2026-08-11 13:08:47 +0700

    Update TritiumFireReaction.cs

--- Content.Server/Atmos/Reactions/TritiumFireReaction.cs
+++ Content.Server/Atmos/Reactions/TritiumFireReaction.cs
@@ -70,10 +70,10 @@ namespace Content.Server.Atmos.Reactions
 
                 // TODO ATMOS Radiation pulse here!
 
-                // Conservation of mass is only a little important.
+                // Maybe conservation of mass is a little important.
                 mixture.AdjustMoles(Gas.Tritium, burnedFuel*0.2);
                 mixture.AdjustMoles(Gas.Oxygen, burnedFuel*0.2);
-              mixture.AdjustMoles(Gas.WaterVapor, burnedFuel*0.6);
+               mixture.AdjustMoles(Gas.WaterVapor, burnedFuel*0.6);
 
                 mixture.ReactionResults[(byte)GasReaction.Fire] += burnedFuel;
             }
